f3f722cfd7ba5b80d7b6ccc08ded0e7880679885
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-02-17 19:46:31 +0200

    Рендер за умовою

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types'
 import css from './Statistics.module.css'
 
 
-export const Statistics = ({title, stats}) => {
+export const Statistics = ({ title, stats }) => {
     return (
        <section className={css.statistics}>
-            <h2 className={css.title}>{title || "Upload stats"}</h2>
-
+            {title && (<h2 className={css.title}>{title}</h2>)}
             <ul className={css.statList}>
                  {stats.map(({id, label, percentage}) => (
                     <li className={css.item }key={id}> 
